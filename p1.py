6f0ec4b861862f5523b6c03e6a6c4f18d79d3805
samtk <samueltkooy@gmail.com>
2018-07-11 08:59:21 +0000

    finish makecards function

--- p1.py
+++ p1.py
@@ -1,4 +1,11 @@
 import random
 
-def makeCards():
+def makeCards(numdecks):
    suit = ["K","Q","J","10","9","8","7","6","5","4","3","2","A"]
+   deck = suit
+   for i in range(0,numdecks-1):
+      for k in range(0,13):
+         deck.append(suit[k])
+   return deck
+
+
