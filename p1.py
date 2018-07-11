6072738457312d3e42d6293470b2e38633dfca94
samtk <samueltkooy@gmail.com>
2018-07-11 15:27:20 +0000

    start to add money

--- p1.py
+++ p1.py
@@ -36,21 +36,24 @@ def getCardValue(card):
 def whoWins(dealervalue,playervalue):
    result = ""
    if(dealervalue > 21 and playervalue > 21):
-      result = "draw"
-   if(dealervalue == playervalue):
-      result ="draw"
-   if(dealervalue > 21):
+      result = "no one"
+   elif(dealervalue == playervalue):
+      result ="no one"
+   elif(dealervalue > 21):
       result = "player"
-   if(playervalue > 21):
+   elif(playervalue > 21):
       result = "dealer"
-   if(playervalue > dealervalue):
+   elif(playervalue > dealervalue):
       result = "player"
    else:
       result = "dealer"
    return result
 
 def game():
+   money = 100
    print("Game starting, type quit to exit program")
+   print("You currently have $" + str(money))
+   bet = input("Enter bet amount (min $10)")
    deck = makeCards(8)
    dealerdraw = random.randint(0,len(deck)-1) 
    playerdraw = random.randint(0,len(deck)-1)
@@ -72,6 +75,8 @@ def game():
    playerhold = False
    dealerhold = False
    while(playerinput.lower() != "quit"):
+      if(playerhandvalue > 21):
+         playerhold = True
       if(playerhold == False):
          playerinput = input("Would you like to hit or miss?  ")
          if(playerinput == "hit"):
@@ -80,12 +85,12 @@ def game():
             cardval = getCardValue(playerhand[len(playerhand)-1])
             if(cardval == 0):
                ace = input("Would you like the ace to be 1 or 11?  ")
-               playerhandvalue = int(ace)
+               playerhandvalue += int(ace)
             else:
                playerhandvalue += int(cardval)
          elif(playerinput == "miss"):
             playerhold = True
-      if(dealerhandvalue < 16):
+      if(dealerhandvalue < 17):
          dealerdraw = random.randint(0,len(deck)-1)
          dealerhand.append(deck[dealerdraw]) 
          cardval = getCardValue(dealerhand[len(dealerhand)-1])
@@ -96,8 +101,8 @@ def game():
       else:
          dealerhold = True    
       drawboard(dealerhand, playerhand, dealerhandvalue, playerhandvalue)
-      if(dealerhold and playerhold):
-         print(whoWins(dealerhandvalue,playerhandalue)) 
+      if((dealerhold and playerhold) or (dealerhandvalue > 21 and playerhandvalue > 21)):
+         print(whoWins(dealerhandvalue,playerhandvalue) + " wins!") 
          dealerhand = []
          playerhand = []
          dealerhandvalue = 0
